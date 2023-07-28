a3a68924af3b686a07de9f9e597f0a2b82f3b17e
snehamoybag <snehamoybag2@gmail.com>
2023-07-28 15:09:58 +0530

    complete removeFromArray task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removableItems) {
+  const updatedArr = arr.filter((currentItem) => {
+    if (!removableItems.includes(currentItem)) {
+      return currentItem;
+    }
+  });
 
+  return updatedArr;
 };
 
 // Do not edit below this line
