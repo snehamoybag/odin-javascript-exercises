c09336491d0c80ecfa962f1ae6d30f9fc0795d13
snehamoybag <snehamoybag2@gmail.com>
2023-07-28 13:57:48 +0530

    write repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+// repeat a string number of times
+const repeatString = function (string, n) {
+  if (n < 0) {
+    return "ERROR";
+  }
 
+  if (n === 0 || !string) {
+    return "";
+  }
+
+  let repeatedString = "";
+  for (let i = 1; i <= n; i++) {
+    repeatedString += string;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
