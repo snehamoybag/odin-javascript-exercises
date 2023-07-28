f22eab7efd566a0d3e80ea9e38a3a62dce0dd18c
snehamoybag <snehamoybag2@gmail.com>
2023-07-28 14:11:45 +0530

    write reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  if (!string) {
+    return "";
+  }
 
+  const allCharactersArr = string.split("");
+  const allCharactersReversedArr = allCharactersArr.reverse();
+  const reversedString = allCharactersReversedArr.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
