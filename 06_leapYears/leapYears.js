4fec80a64aada301843ed78a97945383a3f37ec7
snehamoybag <snehamoybag2@gmail.com>
2023-07-28 20:12:08 +0530

    remove extra spaces

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,11 +1,3 @@
-// check if if it is a century year
-// by doing year % 100 === 0
-// if it is true do year % 400 === 0
-// if the above modular is true it is a leap year
-// if it is false do a year % 4 === 0
-// if true, it is leap year
-// else not a leap year
-
 const leapYears = function(year) {
   const isCenturyYear = year % 100 === 0;
   let isLeapYear = false;
