ceae7e65c95d0893aa776bb4e758adc9e8c00bfc
snehamoybag <snehamoybag2@gmail.com>
2023-07-28 16:28:54 +0530

    finnish sumAll function excercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+// we can use a simple for loop to sum numbers from min till max
+// but this can be less efficient incase of huge numbers
+// we need to find a mathematical formula to sum numbers in range
+// and implement a javascript version of it
 
+// sum consecutive numbers in range
+const sumAll = function(firstNum, secondNum) {
+  if (
+    typeof firstNum !== "number" ||
+    typeof secondNum !== "number" ||
+    firstNum < 0 ||
+    secondNum < 0
+  ) {
+    return "ERROR";
+  }
+
+  const minNum = Math.min(firstNum, secondNum);
+  const maxNum = Math.max(firstNum, secondNum);
+
+  // total numbers in between
+  const n = maxNum - minNum + 1;
+  // math formula of sum of all consecutive numbers is
+  // (n/2 * (maxNum + minNum))
+  const sumOfAllNums = (n / 2) * (maxNum + minNum);
+  return sumOfAllNums;
 };
 
 // Do not edit below this line
