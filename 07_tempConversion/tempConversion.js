b0c86d716faca9d50b01e6ec926e9c8f66d54e36
snehamoybag <snehamoybag2@gmail.com>
2023-07-29 10:40:11 +0530

    complete tempConversation exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // The formula for Fahrenheit to Celsius is °C = (°F − 32) × 5/9.
+  const celsius = (fahrenheit - 32) * (5 / 9);
+  return Math.round(celsius * 10) / 10; // for precision
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // The formula for Celsius to Fahrenheit is °F = (°C × 9/5) + 32.
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return Math.round(fahrenheit * 10) / 10; // for precision
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
