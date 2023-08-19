9fce29d136656709129bc56e87444e05707b2beb
snehamoybag <snehamoybag2@gmail.com>
2023-08-19 23:53:46 +0530

    complete findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const oldestPersonObj = arr.reduce((prevOldestObj, currentObj) => {
+    const currentAgeIfAlive = new Date().getFullYear();
+    const currentPersonAge = (currentObj.yearOfDeath || currentAgeIfAlive) - currentObj.yearOfBirth;
+    const prevPersonAge = (prevOldestObj.yearOfDeath || currentAgeIfAlive) - prevOldestObj.yearOfBirth;
 
+    return currentPersonAge > prevPersonAge ? currentObj : prevOldestObj;
+  });
+
+  return oldestPersonObj;
 };
 
 // Do not edit below this line
