5ba3c3031ee8de3cac4a2a468710383f3c64760a
snehamoybag <snehamoybag2@gmail.com>
2023-08-19 21:20:38 +0530

    complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function(str) {
+  const letterOrNumber = /[a-z0-9]/i;
+  const lowerCaseStr = str.toLowerCase();
+  const arrOfLettersAndNumbers = Array.from(lowerCaseStr).filter(
+    (currentItem) => {
+      return letterOrNumber.test(currentItem);
+    }
+  );
 
+  for (let i = 0; i < arrOfLettersAndNumbers.length; i++) {
+    const currentItemFromStart = arrOfLettersAndNumbers[i];
+    const currentItemFromEnd =
+      arrOfLettersAndNumbers[arrOfLettersAndNumbers.length - (i + 1)]; // sice i starts at 0
+
+    if (currentItemFromStart !== currentItemFromEnd) return false;
+    else continue;
+  }
+
+  // if loop finsh looping, then str is a palindrome
+  return true;
 };
 
 // Do not edit below this line
