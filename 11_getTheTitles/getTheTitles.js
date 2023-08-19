4ad6ba9994a9096203d47e0cb437c10a74c4348b
snehamoybag <snehamoybag2@gmail.com>
2023-08-19 22:42:07 +0530

    complete getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  return arr.map((oBook) => oBook.title);
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
