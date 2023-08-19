deb1ec4c56744b256bced715595e52c0272f4418
snehamoybag <snehamoybag2@gmail.com>
2023-08-19 22:26:34 +0530

    complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  // turn to number if it is a number string
+  num = parseInt(num);
 
+  if (!num || num <= 0) return "OOPS";
+
+  const aFibonacciNums = [0, 1]; // by default
+  for (let i = 0; i <= num; i++) {
+    const nLast = aFibonacciNums[aFibonacciNums.length - 1];
+    const nSecondToLast = aFibonacciNums[aFibonacciNums.length - 2];
+
+    const nCurrentFibonacci = nLast + nSecondToLast;
+    aFibonacciNums.push(nCurrentFibonacci);
+  }
+
+  return aFibonacciNums[num];
 };
 
 // Do not edit below this line
